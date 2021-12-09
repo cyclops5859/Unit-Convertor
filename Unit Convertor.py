f5dcb24826818f16e63d383a84d6cc108e606775
Gen-X-Cyclops5859 <67413980+cyclops5859@users.noreply.github.com>
2021-12-09 18:00:22 +0300

    Add files via upload

---
+++ Unit Convertor.py
@@ -0,0 +1,12 @@
+# Created by Cyclops!
+a = int(input("Input distance in feet: "))
+
+b = a * 12
+c = a / 3.0
+d = a / 5280.0
+
+print ("")
+
+print("The distance in inches is %i inches." % b)
+print("The distance in yards is %.2f yards." % c)
+print("The distance in miles is %.2f miles." % d)
